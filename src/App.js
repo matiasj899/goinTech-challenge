9217f8188d20738fb86058a2552279f2d137fd36
matiasj899 <matiasjuarez899@gmail.com>
2021-09-18 16:30:25 -0300

    add styles to header and cart page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Categories from "./pages/Categories/Categories";
 import Product from "./pages/Product/Product";
-
+import Cart from "./pages/Cart/Cart";
 function App() {
   return (
     <Router>
@@ -10,6 +10,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/categories" component={Categories} />
         <Route exact path="/product/:id" component={Product} />
+        <Route exact path="/cart" component={Cart} />
       </Switch>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import '../components/header.css'
 
-function Header(){
-    return(
-        <h1>Casa</h1>
-    )
+function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <Link to="/" className='nav-logo'>GoinTech</Link>
+          <form>
+          <label>
+              <input type='search'>
+
+              </input>
+          </label>
+          <label>
+              <input type='submit'>
+              </input>
+          </label>
+
+              </form>
+
+         
+          <ul className='navbar-ul'>
+              <li>
+              <Link to="/categories">Categories</Link>
+              </li>
+              <li>
+              <Link to="/cart">Cart</Link>
+              </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
